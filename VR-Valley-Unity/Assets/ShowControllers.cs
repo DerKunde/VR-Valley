7aeb820b8f33b7577bbe9896638c053a284aa8bb
Sean <sean.rindl@stud.hshl.de>
2021-02-20 00:27:36 +0100

    Show Controllers and Teleporting Hints are controllable

---
+++ VR-Valley-Unity/Assets/ShowControllers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ShowControllers : MonoBehaviour
+{
+
+    public bool showControllers = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(var hand in Player.instance.hands)
+        {
+            if (showControllers)
+            {
+                hand.ShowController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
+            }
+            else
+            {
+                hand.HideController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithoutController);
+            }
+        }
+    }
+}
